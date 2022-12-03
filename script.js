7d5f054ca57f9e788a9876e35e8442a13846f6a2
Remy <remy@chaoticmewtral.com>
2022-12-03 16:33:29 -0500

    Add to files
    Added styling to keep form elements in line. Made a couple changes to index for readability. Added function to add book to array from form input.

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 let myLibrary = [];
 
+// constructor function
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+}
+
+// initial books
+const houseCeruleanSea = new Book("The House in the Cerulean Sea", "TJ Klune", 398, true);
+
+myLibrary.push(houseCeruleanSea);
+
+const amariNightBrothers = new Book("Amari and the Night Brothers", "B. B. Alston", 408, true);
+
+myLibrary.push(amariNightBrothers);
+
+// get form data
+const submit = document.querySelector("#submit");
+submit.addEventListener("click", addBook);
+
+// add to array from form input
+function addBook(e) {
+const title = document.querySelector("#title").value;
+const author = document.querySelector("#author").value;
+const pages = document.querySelector("#length").value;
+const read = document.querySelector("#read").checked;
+
+e.preventDefault();
+let newBook = new Book(title, author, pages, read);
+myLibrary.push(newBook);
+document.querySelector("form").reset();
+document.querySelector("#title").focus();
+}
+
+function showBooks() {
+    
 }
